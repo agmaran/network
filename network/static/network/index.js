d6ccdb26381699a27e05c516eafeb1938821195e
ArantxaGarciaMoreno <arantxagarciamoreno@gmail.com>
2021-02-22 16:09:29 -0400

    All Posts

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -27,14 +27,56 @@ class NewPost extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="container col-12">
                 <h2>New Post</h2>
                 <form id="new-post-form" onSubmit={this.post}>
                     <textarea className="form-control" id="new-post-text" value={this.state.text} onChange={this.updateTextArea}></textarea>
-                    <input type="submit" className="btn btn-primary" value="Post" />
+                    <input type="submit" className="btn btn-primary mt-1 mb-1" value="Post" />
                 </form>
             </div>
         );
     }
 }
 ReactDOM.render(<NewPost />, document.querySelector('#new-post'));
+
+class AllPosts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { posts: [], current_user: '' };
+    }
+
+    componentDidMount() {
+        fetch('/allposts')
+            .then(response => response.json())
+            .then(result => {
+                console.log(result)
+                this.setState({ posts: result.posts, current_user: result.current_user })
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    };
+
+    render() {
+        var posts = this.state.posts.map((post) => {
+            return (
+                <div key={post.id} className="container col-12">
+                    <h3>{post.poster}</h3>
+                    {this.state.current_user == post.poster &&
+                        <a href="#">Edit</a>
+                    }
+                    <p>{post.content}</p>
+                    <div className="timestamp">{post.timestamp}</div>
+                    <div className="row">
+                        <i className="bi bi-star ml-3 mr-1"></i>
+                        <div>{post.likes}</div>
+                    </div>
+                </div>
+            )
+        })
+
+        return <div>{posts}</div>
+
+    }
+}
+ReactDOM.render(<AllPosts />, document.querySelector('#all-posts'));
